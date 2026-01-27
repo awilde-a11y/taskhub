bf90267f0b5b9315cc7a978b459f4dde67c3b6b7
awilde-a11y <a.wilde@tqgg.de>
2026-01-27 08:25:23 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
-self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
 
 self.addEventListener("activate", (event) => {
   event.waitUntil((async () => {
+    // löscht ALLE alten Caches
     const keys = await caches.keys();
     await Promise.all(keys.map(k => caches.delete(k)));
     await self.clients.claim();
   })());
 });
 
+// Wichtig: nicht cachen, nicht intercepten – einfach Netzwerk
 self.addEventListener("fetch", () => {});
